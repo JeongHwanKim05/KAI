38315b7b0b82db92d452e5c7b5c187b3fce7adb2
JeongHwanKim <junghan0719@naver.com>
2024-04-13 12:09:39 +0900

    jusuk

--- js/base.js
+++ js/base.js
@@ -1,19 +1,19 @@
 // LoadNAVBAR 함수 선언
 function LoadNAVBAR() {
-    const allElements = document.getElementsByTagName('*');
+    const allElements = document.getElementsByTagName('*');  // 문서의 모든 요소를 가져옴
     for (var i = 0; i < allElements.length; i++) {
         var el = allElements[i];
-        var navpath = el.getAttribute("nav-include-path");
+        var navpath = el.getAttribute("nav-include-path"); //모든요소에 적용
         if (navpath) {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.innerHTML = this.responseText;
-                    el.removeAttribute("nav-include-path");
+                if (this.readyState == 4 && this.status == 200) {   // this는 각각 readyState와 status에 대한 참조?를 한다
+                    el.innerHTML = this.responseText;                   //XMLHttpRequest가 받은 데이터인것같습니다
+                    el.removeAttribute("nav-include-path");   //nav-include-path라는 이름을 사용해서 호출
                     LoadNAVBAR();
                 }
             }
-            xhttp.open("GET", navpath, true);
+            xhttp.open("GET", navpath, true);       // method가 true라면 함수를 실행
             xhttp.send();
             return;
         }
@@ -42,7 +42,7 @@ function LoadFOOTER() {
 
 
 
-// 페이지 로드 시 LoadNAVBAR와 LoadFOOTER 함수를 호출하여 네비게이션 바와 footer를 로드
+// LoadNAVBAR와 LoadFOOTER 함수를 호출하여 네비게이션 바와 footer를 로드
 window.onload = function () {
     LoadNAVBAR();
     LoadFOOTER();
